0381b478d6f73dbfdc49f85f1296368a65120873
Tyson Tate <tyson@doloreslabs.com>
2011-05-24 18:39:03 -0700

    Require rspec before we monkey patch it.

--- lib/scenario.rb
+++ lib/scenario.rb
@@ -1,5 +1,11 @@
 $: << File.expand_path( File.dirname( __FILE__ ) )
 
+begin
+  require "rspec"
+rescue LoadError
+  require "spec"
+end
+
 require 'scenario/fixtures'
 require 'scenario/scenario'
 require 'scenario/rspec'
